0618812b0c4482590bb63473b9438dfa3f02553e
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2024-02-14 12:10:26 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,32 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div>
+      <div className="App">Weather app</div>
+      <footer>
+        Coded 👩‍💻 by{" "}
+        <a
+          href="https://github.com/j4mieLeigh"
+          target="_blank"
+        >
+          Jamie Leigh
+        </a>{" "}
+        | Open-sourced 👋 on{" "}
+        <a
+          href="https://github.com/j4mieLeigh/weather-app-react"
+          target="_blank"
+        >
+          Github
+        </a>{" "}
+        | Hosted 🥳 by{" "}
+        <a
+          href="#"
+          target="_blank"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
