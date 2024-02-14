785c98db29b55ec86d03fc866dceaca85f394f5b
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2024-02-14 13:36:23 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <div className="App">Testing new content</div>
-      <footer>
-        Coded 👩‍💻 by{" "}
-        <a
-          href="https://github.com/j4mieLeigh"
-          target="_blank"
-        >
-          Jamie Leigh
-        </a>{" "}
-        | Open-sourced 👋 on{" "}
-        <a
-          href="https://github.com/j4mieLeigh/weather-app-react"
-          target="_blank"
-        >
-          Github
-        </a>{" "}
-        | Hosted 🥳 by{" "}
-        <a
-          href="#"
-          target="_blank"
-        >
-          Netlify
-        </a>
-      </footer>
+    <div className="App">
+      <div className="container">
+        <h1>Testing new content</h1>
+        <footer>
+          Coded 👩‍💻 by{" "}
+          <a
+            href="https://github.com/j4mieLeigh"
+            target="_blank"
+          >
+            Jamie Leigh
+          </a>{" "}
+          | Open-sourced 👋 on{" "}
+          <a
+            href="https://github.com/j4mieLeigh/weather-app-react"
+            target="_blank"
+          >
+            Github
+          </a>{" "}
+          | Hosted 🥳 by{" "}
+          <a
+            href="https://preeminent-gumdrop-b73ad5.netlify.app/"
+            target="_blank"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
