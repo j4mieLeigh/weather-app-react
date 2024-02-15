68962677c90977bdb7aeb007b9a6173f1d3d3f66
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2024-02-15 15:00:03 +0100

    Updated icon url

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      icon: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
